f38ae37831e66c2afca3ed0f4e8623fc1afaf0d1
Alex Gunnarson <alexandergunnarson@gmail.com>
2016-03-12 15:53:35 -0700

    Improve read-files; added dropr

--- src/clj/nvalt_proto/core.clj
+++ src/clj/nvalt_proto/core.clj
@@ -2,14 +2,25 @@
   (:require [loom.alg       :as galg]
             [clojure.pprint
               :refer [pprint]]
-            [clojure.string :as str ]))
+            [clojure.string :as str])
+  (:import java.io.File))
 
-(defn read-files [path]
+(defn dropr
+  "Drop n elements from right end of a sequence"
+  [n s]
+  (cond (string? s)
+        (subs s 0 (- (count s) n))))
+
+(defn read-files
+  "Slurps the .txt files in a given directory.
+   Outputs a map of <file-name> -> <file-text>"
+  [path]
   (->> path
+       (File.)
        file-seq
-       (filter #(-> % str (str/ends-with? ".txt")))
-       (map    (juxt str slurp))
-       ))
+       (filter #(-> % str (.endsWith #_str/ends-with? ".txt")))
+       (map    (juxt #(->> % .getName (dropr 4)) slurp))
+       (into {})))
 
 (defn tests []
   (pprint (read-files "/Users/alexandergunnarson/Development/Source Code Projects/junto-labs/nvalt-proto/nvalt-proto")))
