b280113d7076792d4f4aa818d957cd9e29b16183
Alex Gunnarson <alexandergunnarson@gmail.com>
2016-03-12 15:49:03 -0700

    Beginning work on main namespace

--- src/clj/nvalt_proto/core.clj
+++ src/clj/nvalt_proto/core.clj
@@ -1,3 +1,17 @@
 (ns nvalt-proto.core
-  (:require [loom.alg :as galg]))
+  (:require [loom.alg       :as galg]
+            [clojure.pprint
+              :refer [pprint]]
+            [clojure.string :as str ]))
 
+(defn read-files [path]
+  (->> path
+       file-seq
+       (filter #(-> % str (str/ends-with? ".txt")))
+       (map    (juxt str slurp))
+       ))
+
+(defn tests []
+  (pprint (read-files "/Users/alexandergunnarson/Development/Source Code Projects/junto-labs/nvalt-proto/nvalt-proto")))
+
+(tests)
